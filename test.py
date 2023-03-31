69be51d34e1e2d0aa851fe0337f9a07aac69a6e8
weibangwei_win <122783533@qq.com>
2023-03-31 13:10:41 +0800

    first

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-print("abc")
+import os
+import openai
+openai.organization = "org-cfA48ke3ieIARV7GtozUc9Zz"
+openai.api_key = "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"
+openai.Model.list()
