7aaa41936db40d8eb545182fb6f5120b7376434a
weibangwei_win <122783533@qq.com>
2023-03-31 13:14:40 +0800

    first

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os
 import openai
 openai.organization = "org-cfA48ke3ieIARV7GtozUc9Zz"
-openai.api_key = "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"
+openai.api_key = "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"
 openai.Model.list()
