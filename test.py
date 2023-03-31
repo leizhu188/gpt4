3f5c370cc175052e3974a1015fc340523af589b9
weibangwei_win <122783533@qq.com>
2023-03-31 13:17:09 +0800

    first

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os
 import openai
 openai.organization = "org-cfA48ke3ieIARV7GtozUc9Zz"
-openai.api_key = "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"
+openai.api_key = "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"
 openai.Model.list()
